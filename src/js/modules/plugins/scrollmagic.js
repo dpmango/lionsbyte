c24f27a017a1471487f63e2cf8ed56430744d4c9
Sergey Khmelevskoy <xs290@me.com>
2021-05-24 20:29:31 +0300

    intro animation

--- src/js/modules/plugins/scrollmagic.js
+++ src/js/modules/plugins/scrollmagic.js
@@ -16,6 +16,7 @@
     scrollTrigger: function () {
       const buildParams = (custom) => {
         const params = {
+          defaults: { duration: 1 },
           markers: {
             startColor: 'forestgreen',
             endColor: 'crimson',
@@ -34,13 +35,57 @@
       const scroll = (x) => `[data-scroll="${x}"]`;
       const section = (x) => `[data-section="${x}"]`;
 
+      const totalVH = _document.height() / _window.innerHeight();
+      const oneVhProgress = 1 / totalVH;
+
+      const $background = $(section('background'));
+      if ($background.length > 0) {
+        let timeline_bg = gsap.timeline({
+          scrollTrigger: buildParams({
+            trigger: 'body',
+            onToggle: (self) => {
+              // if (!self.isActive) {
+              //   $(self.trigger).addClass('is-finnished');
+              // } else {
+              //   $(self.trigger).addClass('is-finnished');
+              // }
+            },
+
+            // end: function () {
+            //   return _window.innerHeight() * 3;
+            // },
+            // pin: true,
+          }),
+        });
+
+        timeline_bg
+          .from(
+            '#separator-line-45-deg',
+            {
+              // rotation: 0,
+              // transformOrigin: '50% 50%',
+              duration: 1,
+            },
+            0
+          )
+          .to(
+            '#separator-line-45-deg',
+            {
+              rotation: 45,
+              transformOrigin: '50% 50%',
+              duration: oneVhProgress * 0.8,
+            },
+            oneVhProgress * 2.3
+          );
+      }
+
       const $intro = $('.intro');
       if ($intro.length > 0) {
         // timeline instance
         let timeline = gsap.timeline({
           scrollTrigger: buildParams({
             trigger: section('intro-first'),
-            pin: `${section('intro-first')} .intro__wrapper`,
+            pin: `${section('intro-first')} [data-pin]`,
             // snap: {
             //   snapTo: 'labels', // snap to the closest label in the timeline
             //   duration: { min: 0.2, max: 3 }, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
@@ -62,19 +107,23 @@
 
         timeline
           .from(scroll('intro-name'), { x: 400 }, 0)
-          .from(scroll('intro-text'), { x: -600 }, 0);
+          .from(scroll('intro-text'), { x: -600 }, 0)
+          .from(scroll('intro-name'), { opacity: 0, duration: 0.2 }, 0)
+          .from(scroll('intro-text'), { opacity: 0, duration: 0.2 }, 0);
 
         // SECOND
         let timeline_2 = gsap.timeline({
           scrollTrigger: buildParams({
             trigger: section('intro-second'),
-            pin: `${section('intro-second')} .container`,
+            pin: `${section('intro-second')} [data-pin]`,
           }),
         });
 
         timeline_2
-          .from(scroll('intro-second-top'), { y: 100 }, 0)
-          .from(scroll('intro-second-bottom'), { y: -100 }, 0);
+          .from(scroll('intro-second-top'), { y: 240 }, 0)
+          .from(scroll('intro-second-bottom'), { y: -240 }, 0)
+          .from(scroll('intro-second-top'), { opacity: 0, duration: 0.2 }, 0)
+          .from(scroll('intro-second-bottom'), { opacity: 0, duration: 0.2 }, 0);
 
         // LION
         let timeline_lion = gsap.timeline({
@@ -83,11 +132,13 @@
             pin: `${section('overlay-lion')} .lottie`,
             onUpdate: (self) => {
               const lionLootie = APP.Components.Lottie.data.lion;
-              const totalFrames = lionLootie.totalFrames;
-              const curFrame = Math.floor(totalFrames * self.progress);
+              if (lionLootie) {
+                const totalFrames = lionLootie.totalFrames;
+                const curFrame = Math.floor(totalFrames * self.progress);
 
-              if (curFrame > 0 && curFrame < totalFrames) {
-                lionLootie.goToAndStop(curFrame, true);
+                if (curFrame > 0 && curFrame < totalFrames) {
+                  lionLootie.goToAndStop(curFrame, true);
+                }
               }
             },
           }),
